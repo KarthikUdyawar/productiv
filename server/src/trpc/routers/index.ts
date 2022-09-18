15f460d0e9a4a6cbaefd5c51b75ac2bafb282201
Karthik Udyawar <karthikajitudy@gmail.com>
2022-09-18 23:42:56 +0530

    Added test api in trpc
    ### Added
    
    - Added new API called test in app Router

--- server/src/trpc/routers/index.ts
+++ server/src/trpc/routers/index.ts
@@ -1,20 +1,29 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
-import { Context } from '../../context';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { Context } from "../../context";
 
 // [...]
 
-export const appRouter = trpc.router<Context>().query('hello', {
-	input: z
-		.object({
-			text: z.string().nullish()
-		})
-		.nullish(),
-	resolve({ input }) {
-		return {
-			greeting: `hello ${input?.text ?? 'world'}`
-		};
-	}
-});
+export const appRouter = trpc
+  .router<Context>()
+  .query("hello", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .query("test", {
+    resolve() {
+      return {
+        message: "Karthik",
+      };
+    },
+  });
 
 export type AppRouter = typeof appRouter;
